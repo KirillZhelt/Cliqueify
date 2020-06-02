3c91093d363008a7cc69e8820dea4567a5a4f3fb
Kirill Zheltovski <kirillzhelt@gmail.com>
2020-06-02 15:13:34 +0300

    Initial commit

---
+++ src/main/java/dev/kirillzhelt/cliqueify/CliqueifyApplication.java
@@ -0,0 +1,13 @@
+package dev.kirillzhelt.cliqueify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliqueifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CliqueifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/kirillzhelt/cliqueify/CliqueifyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kirillzhelt.cliqueify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliqueifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
